711b1d7013e4a1bd823bb595e5d74ab8d6dafd89
Paul Maxwell <paulcmaxwell@gmail.com>
2024-06-16 10:24:39 -0400

    first commit

---
+++ src/ui/Button.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+function Button({ children, disabled, to, type }) {
+  const base =
+    'text-sm smd:px-6 smd:py-4 inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+
+  const styles = {
+    primary: base + 'px-4 py-3 md:px-6 md:py-4',
+    small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'text-sm sm:px-6 sm:py-4 inline-block rounded-full border-2 border-stone-300 bg-transparent  font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:outline-none focus:ring  focus:text-stone-800 hover:text-stone-800 focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
+  };
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+  return (
+    <button to={to} disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
